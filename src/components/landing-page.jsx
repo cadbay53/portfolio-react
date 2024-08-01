8f241592921578a31bddac32346e58802cdfff38
Adnan Bayadwala <codealot53@gmail.com>
2024-08-02 00:52:04 +0530

    colorify added

--- src/components/landing-page.jsx
+++ src/components/landing-page.jsx
@@ -4,7 +4,7 @@ export default function LandingPage(){
         <div>
             <h1>My practice apps</h1>
             <a style={{fontSize:"30px"}} href="https://adnantodo.netlify.app/" target="blank">Todo app</a><br />
-            <a style={{fontSize:"30px"}} href="">Dummy app</a><br />
+            <a style={{fontSize:"30px"}} href="https://colorify-tr.netlify.app/">Colorify</a><br />
             <a style={{fontSize:"30px"}} href="">Dummy app</a><br />
         </div>
     )
