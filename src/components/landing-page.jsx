9c9148098186b349ec8902bc0c5f9f5003a32902
Adnan Bayadwala <codealot53@gmail.com>
2024-08-01 13:51:16 +0530

    portfolio from netlify

---
+++ src/components/landing-page.jsx
@@ -0,0 +1,11 @@
+export default function LandingPage(){
+    return(
+
+        <div>
+            <h1>My practice apps</h1>
+            <a style={{fontSize:"30px"}} href="https://adnantodo.netlify.app/" target="blank">Todo app</a><br />
+            <a style={{fontSize:"30px"}} href="">Todo app</a><br />
+            <a style={{fontSize:"30px"}} href="">Todo app</a><br />
+        </div>
+    )
+}
