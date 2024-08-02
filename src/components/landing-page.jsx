d0b15455d103f44dc56325417aa0ee8429096d8b
Adnan Bayadwala <codealot53@gmail.com>
2024-08-02 13:30:35 +0530

    removed url github.io

--- src/components/landing-page.jsx
+++ src/components/landing-page.jsx
@@ -4,7 +4,7 @@ export default function LandingPage(){
         <div>
             <h1>My practice apps</h1>
             <a style={{fontSize:"30px"}} href="https://adnantodo.netlify.app/" target="blank">Todo app</a><br />
-            <a style={{fontSize:"30px"}} href="https://cadbay53.github.io/">Colorify</a><br />
+            <a style={{fontSize:"30px"}} href="https://colorify.netlify.app/">Colorify</a><br />
             <a style={{fontSize:"30px"}} href="">Dummy app</a><br />
         </div>
     )
