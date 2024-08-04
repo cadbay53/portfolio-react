7f6f443a987ff129b209d8d9421c12db268a1f8f
Adnan Bayadwala <codealot53@gmail.com>
2024-08-05 03:29:41 +0530

    new todo added

--- src/components/landing-page.jsx
+++ src/components/landing-page.jsx
@@ -3,7 +3,7 @@ export default function LandingPage(){
 
         <div>
             <h1>My practice apps</h1>
-            <a style={{fontSize:"30px"}} href="https://adnantodo.netlify.app/" target="blank">Todo app</a><br />
+            <a style={{fontSize:"30px"}} href="https://cadbay-todo.netlify.app/" target="blank">Todo app</a><br />
             <a style={{fontSize:"30px"}} href="https://colorify-tr.netlify.app/">Colorify</a><br />
             <a style={{fontSize:"30px"}} href="">Dummy app</a><br />
         </div>
